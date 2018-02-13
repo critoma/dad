28cabf640ed03cf6855ab5806f5d868995bddd0d
critoma <cristian.toma@ie.ase.ro>
2018-02-14 00:58:48 +0200

    Add the lab for the Java REST - JAX-RS

---
+++ labs/s08_jweb_rest/src/eu/ase/rest/Track.java
@@ -0,0 +1,28 @@
+package eu.ase.rest;
+
+public class Track {
+	String title;
+	String singer;
+	
+	public Track() {
+		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSinger() {
+		return singer;
+	}
+
+	public void setSinger(String singer) {
+		this.singer = singer;
+	}
+	
+	
+}
