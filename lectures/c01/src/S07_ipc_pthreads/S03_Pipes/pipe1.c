bfc14573ca73f4546ce9eb72566f4b0c9c2d7443
critoma <cristian.toma@ie.ase.ro>
2020-02-19 12:37:17 +0200

    Reshape pipe example

--- lectures/c01/src/S07_ipc_pthreads/S03_Pipes/pipe1.c
+++ lectures/c01/src/S07_ipc_pthreads/S03_Pipes/pipe1.c
@@ -11,14 +11,16 @@ int main()
         pipe(pfds);
 
         if (!fork()) {
-            printf(" CHILD: writing to the pipe\n");
+            printf(" CHILD: writing to the pipe %d\n", pfds[1]);
             write(pfds[1], "test", 5);
+	    printf(" CHILD: pipe assigned for read = %d\n", pfds[0]);
             printf(" CHILD: exiting\n");
             exit(0);
         } else {
-            printf("PARENT: reading from pipe\n");
+            printf("PARENT: reading from pipe %d\n", pfds[0]);
             read(pfds[0], bufread, 5);
             printf("PARENT: read \"%s\"\n", bufread);
+	    printf(" PARENT: pipe assigned for write = %d\n", pfds[1]);
             wait(NULL);
         }
 }
