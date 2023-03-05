9076fc1d52dc92de5691a3d50f8ae548402776a8
critoma <cristianvtoma@gmail.com>
2023-03-05 09:48:58 +0200

    Update clienttcp.js

--- lectures/c09/nodejs/clienttcp.js
+++ lectures/c09/nodejs/clienttcp.js
@@ -1,7 +1,7 @@
 var net = require('net');
 
-var client = net.connect({port: 8090}, '127.0.0.1', function () {
-//var client = net.connect({port: 8090}, '172.20.10.3', function () {
+//var client = net.connect({port: 8090}, '127.0.0.1', function () {
+var client = net.connect(8090, '172.20.10.3', function () {
    console.log("client conncted to the server!");
    client.write('Hello, server! I am the client Chuck Norris...');
 });
