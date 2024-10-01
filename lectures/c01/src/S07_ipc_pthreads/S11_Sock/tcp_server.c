15dc76e22df44a25f99167c513eb7327cb58ce94
critoma <cristianvtoma@gmail.com>
2024-10-01 10:01:33 +0300

    Create tcp_server.c

---
+++ lectures/c01/src/S07_ipc_pthreads/S11_Sock/tcp_server.c
@@ -0,0 +1,49 @@
+// gcc -o tcp_server.elf tcp_server.c
+// ./tcp_server.elf 5000
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h> 
+
+int main(int argc, char *argv[])
+{
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr; 
+
+    char sendBuff[1025];
+    time_t ticks; 
+    if (argc != 2)
+        printf("\n TCP Usage: %s <port> \n", argv[0]);
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff)); 
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1])); 
+
+    int ret_bind = bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
+    if (ret_bind != 0) 
+        printf("\nbinding on port failed!\n");
+
+    listen(listenfd, 10); 
+
+    while(1)
+    {
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
+
+        ticks = time(NULL);
+        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+        write(connfd, sendBuff, strlen(sendBuff)); 
+
+        close(connfd);
+        sleep(1);
+     }
+}
