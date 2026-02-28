991190e3a178517f770513d47bdfc18f9ce62171
critoma <cristianvtoma@gmail.com>
2026-02-28 13:45:00 +0200

    Update events.js

--- lectures/c09/nodejs/events.js
+++ lectures/c09/nodejs/events.js
@@ -13,8 +13,7 @@ eventEmitter.emit('connection');
 console.log("Program Finished!");
 */
 
-
-
+/*
 var events = require('events');
 
 var eventEmitter = new events.EventEmitter();
@@ -32,6 +31,36 @@ eventEmitter.on('data_received', function () {
 
 eventEmitter.emit('connection');
 
+console.log("Program Finished!");
+*/
+
+/*
+let myEventsModule = require("events");
+let objEventEmitter = new myEventsModule.EventEmitter();
+let fcnHandler = function connectionNow() {
+   setTimeout(() => {
+     console.log("Event handler with success");
+   }, 1000);
+};
+objEventEmitter.on("myEventNow01", fcnHandler);
+objEventEmitter.emit("myEventNow01");
+console.log("prg events finished!");
+*/
+
+let events = require("events");
+let eventEmitter = new events.EventEmitter();
+let connectHandler = function connected() {
+  console.log("connection with success!");
+  setTimeout(() => {
+    console.log("wait 1 sec");
+    eventEmitter.emit("data_received");
+  }, 1000);
+};
+eventEmitter.on("connection", connectHandler);
+eventEmitter.on("data_received", () => {
+  console.log("Data received with success!");
+});
+eventEmitter.emit("connection");
 console.log("Program Finished!");
 
 
